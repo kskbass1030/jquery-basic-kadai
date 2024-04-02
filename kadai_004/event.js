344aa2cfee50b5fe39bf38d14f42e75a1357cc38
kskbass1030 <153488051+kskbass1030@users.noreply.github.com>
2024-04-02 14:53:39 +0900

    kadai_004の提出

---
+++ kadai_004/event.js
@@ -0,0 +1,17 @@
+// $(window).on('load',() => {
+//   console.log('loadイベントが発生しました');
+// });
+
+// $(window).on('scroll',() => {
+//   console.log('scrollイベントが発生しました');
+// });
+
+$(window).on({
+  'load': () => {
+    console.log('loadイベントが発生しました');
+  },
+
+  'scroll': () => {
+    console.log('scrollイベントが発生しました');
+  }
+});
